aef76d05200e88cf97c589d9920941b0acb1662a
Nikhil <Nikhil@DESKTOP-0Q9LF51>
2022-09-21 17:45:39 +0530

    4

--- Advanced/src/telusko/FinalKey.java
+++ Advanced/src/telusko/FinalKey.java
@@ -1,10 +1,37 @@
 package telusko;
+class Aa{
+	final int NUM;
+	public Aa()
+	{
+		NUM=10;
+	}
+	public void show()
+	{
+		System.out.println("in final class");
+	}
+	final public void showfinal()
+	{
+		System.out.println("in final class showfinal()");
+	}
+}
+
+class Bb extends Aa
+{
+//	void showfinal()        final method cannot be overrided
+//	{
+//		
+//	}
+}
 
 public class FinalKey {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		final double PI = 3.14;
+		
+		Bb obj1 = new Bb();
+		obj1.show();
+		obj1.showfinal();
 	}
 
 }
